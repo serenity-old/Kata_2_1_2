5932a89171e8c4768c50f94fa925dfef64216deb
Oleg <oleg-step@yandex.ru>
2022-05-01 18:55:22 +0300

    first commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    public String name;
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
